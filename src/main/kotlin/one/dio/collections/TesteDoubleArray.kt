7a6e2f03a5f613042ee41e9229e649b4ff0f9e68
PRPAJ <paulo.pj.alves@gmail.com>
2022-06-22 19:03:01 -0300

    Created test Array of and Array String

---
+++ src/main/kotlin/one/dio/collections/TesteDoubleArray.kt
@@ -0,0 +1,25 @@
+package one.dio.collections
+
+fun main(){
+    val salarios = DoubleArray(size = 3)
+    salarios[0] = 1000.0
+    salarios[1] = 3000.0
+    salarios[2] = 500.0
+
+    salarios.forEach { println(it) }
+    println("---------------------")
+    salarios.sort()
+    salarios.forEach { println(it) }
+    println("---------------------")
+    salarios.sortDescending()
+    salarios.forEach { println(it) }
+    println("---------------------")
+    salarios.forEachIndexed {index, salario ->
+        salarios[index] = salario * 1.1
+    }
+    salarios.forEach { println(it) }
+    println("---------------------")
+    val salarios2 = doubleArrayOf(1500.0,1000.0,8000.0)
+    salarios2.sort()
+    salarios2.forEach { println(it) }
+}
