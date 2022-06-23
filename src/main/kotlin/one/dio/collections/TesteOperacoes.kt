830a162e810224a413d6cc88d8be9d74fd8fa7e0
PRPAJ <paulo.pj.alves@gmail.com>
2022-06-23 15:19:45 -0300

    filter, find e any

--- src/main/kotlin/one/dio/collections/TesteOperacoes.kt
+++ src/main/kotlin/one/dio/collections/TesteOperacoes.kt
@@ -7,10 +7,20 @@ fun main(){
         println(salario)
     }
     println("----------------")
-    println("O maior salário é ${salarios.max()}")
-    println("O menor salário é ${salarios.min()}")
+    println("O maior salário é ${salarios.maxOrNull()}")
+    println("O menor salário é ${salarios.minOrNull()}")
     println("A média salarial é ${salarios.average()}")
     println("----------------")
     val salariosMaiorQue2500 = salarios.filter{it>2500}
     salariosMaiorQue2500.forEach {println(it)}
+    println("----------------")
+    println(salarios.filter { it in 2000.0..5000.0 })
+    println("----------------")
+    println(salarios.filter { it in 2000.0..5000.0 })
+    println(salarios.find{it == 2250.0})
+    println(salarios.find{it == 250.0})
+    println("----------------")
+    println(salarios.any{it == 2250.0})
+
+
 }
